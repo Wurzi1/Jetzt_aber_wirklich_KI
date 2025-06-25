b7f73d68a0e52521506c4d83c322eab4279967d0
Daniel <daniel@c-bruhn.de>
2025-06-25 17:34:09 +0200

    Erstes Setup

---
+++ Main/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by danie on 25.06.2025.
+//
+#include "main.h"
+#include <iostream>
+int inputn, hiddenn, outputn;
+double learningreate;
+
+    int main(int input, int hidden, int output, double learningrate) {
+    inputn = input;             //Anzahl der Nodes im jeweiligen Layer
+    hiddenn = hidden;
+    outputn = output;
+    learningreate = learningrate;
+
+    std::cout << "Hello World!";
+
+
+
+
+return 0;
+}
---
+++ Main/main.h
@@ -0,0 +1,20 @@
+//
+// Created by danie on 25.06.2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+
+
+
+    
+};
+
+
+
+#endif //MAIN_H
